c27594f1744fa82305565a68ccbb1c50fe115dcb
AshidaSRS <ashida.shin@gmail.com>
2015-10-05 23:05:14 +0200

    Meh

--- t-bot/one4all.go
+++ t-bot/one4all.go
@@ -7,8 +7,13 @@ import(
 	"strings"
 	"sync/atomic"
 )
-
-type TgBot struct {
+//Telegram constants
+const(
+  APIUrl = "https://api.telegram.org/bot%s/%s"
+  FileUrl = "https://api.telegram.org/file/bot%s/%s"
+)
+//Struct to manage all the interactions with the api
+type TelegramBot struct {
 	Token                string
 	FirstName            string
 	ID                   int
@@ -25,12 +30,26 @@ type TgBot struct {
 	BuildingChain        bool
 	DefaultOptions       DefaultOptionsBot
 }
-
-func New(token string) *TgBot {
-	bot, err := NewWithError(token)
-	if err != nil {
-		panic(err)
+//New bot
+func New(token string) (*TgBot, error) {
+  aurl := fmt.Sprintf(baseURL, token, "%s")
+	furl := fmt.Sprintf(fileURL, token, "%s")
+  tgbot := &TelegramBot{
+		Token:                token,
+		BaseRequestURL:       aurl,
+		BaseFileRequestURL:   furl,
+		MainListener:         nil,
+		RelicCfg:             nil,
+		BotanIO:              nil,
+		TestConditionalFuncs: make([]ConditionCallStructure, 0),
+		NoMessageFuncs:       make([]NoMessageCall, 0),
+		ChainConditionals:    make([]*ChainStructure, 0),
+		BuildingChain:        false,
+		DefaultOptions: DefaultOptionsBot{
+			CleanInitialUsername:       true,
+			AllowWithoutSlashInMention: true,
+		},
 	}
-	return bot
+
 }
 
